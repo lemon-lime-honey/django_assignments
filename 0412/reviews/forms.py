3604b956bd7818dde0198913aa4e5f552dbe6077
lemon-lime-honey <bs0308@g.skku.edu>
2023-04-12 17:29:48 +0900

    Add 0412 and Revise 0411

---
+++ 0412/reviews/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Review, Comment
+
+
+class ReviewForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '제목을 입력하세요',
+            }
+        )
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '내용을 입력하세요',
+                'style': 'height: 10em;',
+            }
+        )
+    )
+    movie = forms.CharField(
+        label='영화',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '영화 제목을 입력하세요',
+            }
+        )
+    )
+
+    class Meta:
+        model = Review
+        fields = ('title', 'content', 'movie',)
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        label='댓글',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '댓글 내용을 입력하세요',
+            }
+        )
+    )
+    class Meta:
+        model = Comment
+        fields = ('content',)
---
+++ 0412/reviews/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField(null=False)
+    movie = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.review)
